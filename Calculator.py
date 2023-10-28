225c79ae539ecc75dd684657fb93bace22ed01b1
Danemmy <nnaemekaokonkwodaniel@gmail.com>
2023-10-29 00:07:02 +0200

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,8 @@
+#Declared a Variable to ask the user for the first number  
 num1 = int(input("Type the first number: "))
+# Declared a variable to ask the user to select an operand 
 Calculation = input("please select an operator, +, -, /, *: ")
+# Declared a variable to ask the user to input a second number 
 num2 = int(input("Type your second number: "))
 
 
